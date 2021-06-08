79702926ce5bc317f4196c3756d1bf4821123c36
dewathiwat <Athiwat_Jansomwong@cmu.ac.th>
2021-06-08 14:57:29 +0700

    even

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-document.getElementById('ok').addEventListener('click',function(e){
+// // document.getElementById('ok').addEventListener('click',function(e){
     
-    alert("OK");
-    console.log(e)
-})
-document.getElementById("cancel").addEventListener( 'mousemove',function(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    var coor = "(X : " + x + ", Y : " + y + ")";
-    if(e.shiftKey)
-    console.log(coor)
-  })
-document.getElementById('cancel').addEventListener('click',function(e){
-    alert("Cancel");
-    console.log(e)
-})
-document.getElementById('no').addEventListener('click',function(e){
-    alert("No");
-    console.log(e)
-})
+// //     alert("OK");
+// //     console.log(e)
+// // })
+// document.getElementById("cancel").addEventListener( 'mousemove',function(e) {
+//     var x = e.clientX;
+//     var y = e.clientY;
+//     var coor = "(X : " + x + ", Y : " + y + ")";
+//     if(e.shiftKey)
+//     console.log(coor)
+//   })
+// document.getElementById('cancel').addEventListener('click',function(e){
+//     alert("Cancel");
+//     console.log(e)
+// })
+// document.getElementById('no').addEventListener('click',function(e){
+//     alert("No");
+//     console.log(e)
+// })
 document.getElementById('ok').addEventListener('click',function (e){
     // document.getElementById('no').classList.add('toggleOn')
     // console.log(document.getElementById('no'))
@@ -49,3 +49,28 @@ cancelElement.addEventListener('mouseover',function (e){
 cancelElement.addEventListener('mouseleave',function (e){
     cancelElement.innerHTML=`cancel`
 })
+var mar=0
+document.getElementById('ok').addEventListener('click',function (e){
+    document.getElementById('cancel').style.margin = `0px ${mar}px 0px ${mar}px`
+    mar=mar+5
+})
+    var n0 = 1
+    var n=''
+document.getElementById('no').addEventListener('dblclick',function (e){
+    var n1 ='วิชานี้'
+    var n2 = 'ง่าย'
+    var n3 = 'จริงๆ นะ'
+    
+    if(n0==1){
+        n=n1
+        n0++
+    }else if(n0==2){
+        n=n2
+        n0++
+    }else if(n0==3){
+        n=n3
+        n0=1
+    }
+    document.getElementById('ok').innerHTML= n
+})
+
