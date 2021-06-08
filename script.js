807a99219b9cbc6128ae88652b99a381fa028bd9
dewathiwat <Athiwat_Jansomwong@cmu.ac.th>
2021-06-08 14:26:41 +0700

    8-6-64

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('ok').addEventListener('click',function(e){
+    
     alert("OK");
     console.log(e)
 })
@@ -6,6 +7,7 @@ document.getElementById("cancel").addEventListener( 'mousemove',function(e) {
     var x = e.clientX;
     var y = e.clientY;
     var coor = "(X : " + x + ", Y : " + y + ")";
+    if(e.shiftKey)
     console.log(coor)
   })
 document.getElementById('cancel').addEventListener('click',function(e){
@@ -15,4 +17,35 @@ document.getElementById('cancel').addEventListener('click',function(e){
 document.getElementById('no').addEventListener('click',function(e){
     alert("No");
     console.log(e)
+})
+document.getElementById('ok').addEventListener('click',function (e){
+    // document.getElementById('no').classList.add('toggleOn')
+    // console.log(document.getElementById('no'))
+    let noElement =document.getElementById('no')
+    if (noElement.classList.contains('toggleOn')){
+        noElement.classList.replace('toggleOn','toggleOff')
+    }else if(noElement.classList.contains('toggleOff')){
+        noElement.classList.replace('toggleOff','toggleOn')
+    }else{
+        noElement.classList.add('toggleOn')
+    }
+
+})
+var borderTick = 20
+document.getElementById('cancel').style.border = `${borderTick}px solid white`
+
+var cancelElement=document.getElementById('cancel')
+cancelElement.addEventListener('mouseover',function (e){
+    cancelElement.innerHTML=`
+    <div class="container-col">
+        <div class="flex-item">
+            no1
+        </div>
+        <div class="flex-item">
+            no2
+        </div>
+    </div>`
+})
+cancelElement.addEventListener('mouseleave',function (e){
+    cancelElement.innerHTML=`cancel`
 })
