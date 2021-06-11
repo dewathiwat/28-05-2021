b6c913e21c18991e518078fbc5cc42b24ea7d0ab
dewathiwat <Athiwat_Jansomwong@cmu.ac.th>
2021-06-11 09:45:28 +0700

    11-06-2021

--- script.js
+++ script.js
@@ -18,59 +18,76 @@
 //     alert("No");
 //     console.log(e)
 // })
-document.getElementById('ok').addEventListener('click',function (e){
-    // document.getElementById('no').classList.add('toggleOn')
-    // console.log(document.getElementById('no'))
-    let noElement =document.getElementById('no')
-    if (noElement.classList.contains('toggleOn')){
-        noElement.classList.replace('toggleOn','toggleOff')
-    }else if(noElement.classList.contains('toggleOff')){
-        noElement.classList.replace('toggleOff','toggleOn')
-    }else{
-        noElement.classList.add('toggleOn')
-    }
+// document.getElementById('ok').addEventListener('click',function (e){
+//     // document.getElementById('no').classList.add('toggleOn')
+//     // console.log(document.getElementById('no'))
+//     let noElement =document.getElementById('no')
+//     if (noElement.classList.contains('toggleOn')){
+//         noElement.classList.replace('toggleOn','toggleOff')
+//     }else if(noElement.classList.contains('toggleOff')){
+//         noElement.classList.replace('toggleOff','toggleOn')
+//     }else{
+//         noElement.classList.add('toggleOn')
+//     }
 
-})
-var borderTick = 20
-document.getElementById('cancel').style.border = `${borderTick}px solid white`
+// })
+// var borderTick = 20
+// document.getElementById('cancel').style.border = `${borderTick}px solid white`
 
-var cancelElement=document.getElementById('cancel')
-cancelElement.addEventListener('mouseover',function (e){
-    cancelElement.innerHTML=`
-    <div class="container-col">
-        <div class="flex-item">
-            no1
-        </div>
-        <div class="flex-item">
-            no2
-        </div>
-    </div>`
-})
-cancelElement.addEventListener('mouseleave',function (e){
-    cancelElement.innerHTML=`cancel`
-})
-var mar=0
-document.getElementById('ok').addEventListener('click',function (e){
-    document.getElementById('cancel').style.margin = `0px ${mar}px 0px ${mar}px`
-    mar=mar+5
-})
-    var n0 = 1
-    var n=''
-document.getElementById('no').addEventListener('dblclick',function (e){
-    var n1 ='วิชานี้'
-    var n2 = 'ง่าย'
-    var n3 = 'จริงๆ นะ'
+// var cancelElement=document.getElementById('cancel')
+// cancelElement.addEventListener('mouseover',function (e){
+//     cancelElement.innerHTML=`
+//     <div class="container-col">
+//         <div class="flex-item">
+//             no1
+//         </div>
+//         <div class="flex-item">
+//             no2
+//         </div>
+//     </div>`
+// })
+// cancelElement.addEventListener('mouseleave',function (e){
+//     cancelElement.innerHTML=`cancel`
+// })
+// var mar=0
+// document.getElementById('ok').addEventListener('click',function (e){
+//     document.getElementById('cancel').style.margin = `0px ${mar}px 0px ${mar}px`
+//     mar=mar+5
+// })
+//     var n0 = 1
+//     var n=''
+// document.getElementById('no').addEventListener('dblclick',function (e){
+//     var n1 ='วิชานี้'
+//     var n2 = 'ง่าย'
+//     var n3 = 'จริงๆ นะ'
     
-    if(n0==1){
-        n=n1
-        n0++
-    }else if(n0==2){
-        n=n2
-        n0++
-    }else if(n0==3){
-        n=n3
-        n0=1
-    }
-    document.getElementById('ok').innerHTML= n
-})
+//     if(n0==1){
+//         n=n1
+//         n0++
+//     }else if(n0==2){
+//         n=n2
+//         n0++
+//     }else if(n0==3){
+//         n=n3
+//         n0=1
+//     }
+//     document.getElementById('ok').innerHTML= n
+// })
+function onOkClicked(e){
+    alert('Ok clicked')
+}
+
+function onCancelClicked(e){
+    alert('Cancel clicked')
+}
+function onNoClicked(e){
+    alert('No clicked')
+}
+document.getElementById('cancel').addEventListener('click',onCancelClicked)
+document.getElementById('no').onclick=onNoClicked
+document.getElementById('ok').onclick=onOkClicked
+document.getElementById('container').onclick=function(e){
+    alert('container clicked')
+}
+
 
